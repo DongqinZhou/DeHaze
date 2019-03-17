99846f841e945393e86a32e6048128ce531338e9
Dongqin Zhou <dongqinzhou1110@gmail.com>
2019-03-17 20:50:03 +0800

    opencv -> opencv-python

---
+++ AOD-Net/aodtest.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import cv2
+image = cv2.imread(r'H:\Undergraduate\18-19-3\Undergraduate Thesis\Dataset\test_images\forest1.jpg')
+cv2.imshow('image', image)
+#cv2.waitKey(27)
--- DCP/Main.py
+++ DCP/Main.py
@@ -6,13 +6,13 @@ Created on Wed Mar 13 17:04:38 2019
 """
 
 import cv2
-import DCP_GF
+import DCP_GF_Final
 im_path = "H:\\Undergraduate\\18-19-3\\Undergraduate Thesis\\Literature\\images\\Org images\\gugong.bmp"
 im = cv2.imread(im_path)
 cv2.imshow('image',im)
-im_dehaze = DCP_GF.dehaze(im_path)
-for i in range(5):
-    cv2.imshow('image' + str(i), im_dehaze[i])
+#im_dehaze = DCP_GF_Final.dehaze(im_path)
+#for i in range(5):
+#    cv2.imshow('image' + str(i), im_dehaze[i])
     
     
     
