2d14ede0a00680511994cc5866a5bb3426662fee
Dongqin Zhou <dongqinzhou1110@gmail.com>
2019-04-17 17:40:52 +0800

    begin customizing linear_comb layer

--- MSCNN/MSCNN_1.py
+++ MSCNN/MSCNN_1.py
@@ -3,7 +3,7 @@ import numpy as np
 import random
 import os
 
-from keras.layers import Conv2D, Input, concatenate, multiply, subtract, Lambda, MaxPooling2D
+from keras.layers import Conv2D, Input, UpSampling2D, concatenate, multiply, subtract, Lambda, MaxPooling2D
 from keras import optimizers
 from keras.models import Model
 import keras.backend as K
@@ -11,10 +11,7 @@ from keras.activations import relu
 
 
 def load_data(data_files,label_files, height, width):
-    '''
-    label_path: the path where all label images locate
-    data_path: the path where a batch of hazy images locate 
-    '''
+    
     data = []
     label = []
     
@@ -28,7 +25,6 @@ def load_data(data_files,label_files, height, width):
             clear_image = cv2.resize(clear_image, (width, height), interpolation = cv2.INTER_AREA)
         data.append(hazy_image)
         label.append(clear_image)
-#    data = np.array(data,dtype="float")/255.0#归一化
     
     data = np.asarray(data) / 255.0
     label = np.asarray(label) / 255.0
@@ -43,11 +39,19 @@ def get_batch(data_files, label_files, batch_size, height, width):
             
             yield x, y
 
-def MSCNN():
+def coarse_net():
     input_image = Input(shape = (None, None, 3))
     conv1 = Conv2D(5, (11,11), strides=(1, 1), padding='valid', activation='relu',kernel_initializer='random_normal')(input_image)
-    mp1 = MaxPooling2D(pool_size = (2,2), padding = 'valid')
-
+    mp1 = MaxPooling2D(pool_size = (2,2), padding = 'valid')(conv1)
+    up1 = UpSampling2D(size=(2,2), interpolation = 'nearest')(mp1)
+    conv2 = Conv2D(5, (9,9), strides=(1, 1), padding='valid', activation='relu',kernel_initializer='random_normal')(up1)
+    mp2 = MaxPooling2D(pool_size = (2,2), padding = 'valid')(conv2)
+    up2 = UpSampling2D(size=(2,2), interpolation = 'nearest')(mp2)
+    conv3 = Conv2D(10, (7,7), strides=(1, 1), padding='valid', activation='relu',kernel_initializer='random_normal')(up2)
+    mp3 = MaxPooling2D(pool_size = (2,2), padding = 'valid')(conv3)
+    up3 = UpSampling2D(size=(2,2), interpolation = 'nearest')(mp3)
+    #linear combination has a sigmoid!
+    
 
 
 
