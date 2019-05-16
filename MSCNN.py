d4879ce0f0cb5aa48321c663384e3388f81fe5fb
Dongqin Zhou <dongqinzhou1110@gmail.com>
2019-05-16 16:33:07 +0800

    MSCNN coded as a single network again

--- MSCNN.py
+++ MSCNN.py
@@ -92,7 +92,7 @@ def get_radiance(hazy_image, airlight, trans_map, L):
     
     return clear_image
     
-def coarseNet():
+def MSCNN():
     input_image = Input(shape = (None, None, 3), name = 'c_input')
     c_conv1 = Conv2D(5, (11,11), strides=(1, 1), padding='same', activation='relu',kernel_initializer='random_normal', name = 'c_conv1')(input_image)
     c_mp1 = MaxPooling2D(pool_size = (2,2), padding = 'valid', name = 'c_mp1')(c_conv1)
@@ -104,19 +104,14 @@ def coarseNet():
     c_mp3 = MaxPooling2D(pool_size = (2,2), padding = 'valid', name = 'c_mp3')(c_conv3)
     c_up3 = UpSampling2D(size=(2,2), interpolation = 'nearest', name = 'c_up3')(c_mp3)
     ### if self defined layer does not work, consider using this convolutional layer
-    c_linear = Conv2D(1, (1,1), strides=(1,1), padding ='same', activation='sigmoid',kernel_initializer='random_normal', name = 'c_linear')(c_up3)
+    #c_linear = Conv2D(1, (1,1), strides=(1,1), padding ='same', activation='sigmoid',kernel_initializer='random_normal', name = 'c_linear')(c_up3)
     ###
-    #c_linear = Linear_Comb(1, name = 'c_linear')(c_up3)
-    model = Model(inputs = input_image, outputs = c_linear)
-    return model
-
-def fineNet(coarseNet):
-    for layer in coarseNet.layers:
-        layer.trainable = False
-    f_conv1 = Conv2D(4, (7,7), strides=(1, 1), padding='same', activation='relu',kernel_initializer='random_normal', name = 'f_conv1')(coarseNet.input)
+    c_linear = Linear_Comb(1, name = 'c_linear')(c_up3)
+   
+    f_conv1 = Conv2D(4, (7,7), strides=(1, 1), padding='same', activation='relu',kernel_initializer='random_normal', name = 'f_conv1')(input_image)
     f_mp1 = MaxPooling2D(pool_size = (2,2), padding = 'valid', name = 'f_mp1')(f_conv1)
     f_up1 = UpSampling2D(size=(2,2), interpolation = 'nearest', name = 'f_up1')(f_mp1)
-    concat1 = concatenate([f_up1, coarseNet.get_layer(name = 'c_linear').output], axis = -1, name = 'f_concat')
+    concat1 = concatenate([f_up1, c_linear], axis = -1, name = 'f_concat')
     f_conv2 = Conv2D(5, (5,5), strides=(1, 1), padding='same', activation='relu',kernel_initializer='random_normal', name = 'f_conv2')(concat1)
     f_mp2 = MaxPooling2D(pool_size = (2,2), padding = 'valid', name = 'f_mp2')(f_conv2)
     f_up2 = UpSampling2D(size=(2,2), interpolation = 'nearest', name = 'f_up2')(f_mp2)
@@ -124,31 +119,24 @@ def fineNet(coarseNet):
     f_mp3 = MaxPooling2D(pool_size = (2,2), padding = 'valid', name = 'f_mp3')(f_conv3)
     f_up3 = UpSampling2D(size=(2,2), interpolation = 'nearest', name = 'f_up3')(f_mp3)
     ### if self defined layer does not work, consider using this convolutional layer
-    f_linear = Conv2D(1, (1,1), strides=(1,1), padding ='same', activation='sigmoid',kernel_initializer='random_normal', name = 'f_linear')(f_up3)
+    #f_linear = Conv2D(1, (1,1), strides=(1,1), padding ='same', activation='sigmoid',kernel_initializer='random_normal', name = 'f_linear')(f_up3)
     ###
-    #f_linear = Linear_Comb(1, name= 'f_linear')(f_up3)
-    model = Model(inputs = coarseNet.input, outputs = f_linear)
+    f_linear = Linear_Comb(1, name= 'f_linear')(f_up3)
+    model = Model(inputs = input_image, outputs = f_linear)
     return model
 
 def train_model(data_path, label_path, weights_path, lr=0.001, momentum=0.9, decay=5e-4, p_train = 0.8, 
                 width = 320, height = 240, batch_size = 48, nb_epochs = 40):
     
-    def c_scheduler(epoch):
+    def scheduler(epoch):
         if epoch % 10 == 0 and epoch != 0:
-            lr = K.get_value(c_sgd.lr)
-            K.set_value(c_sgd.lr, lr * 0.1)
+            lr = K.get_value(sgd.lr)
+            K.set_value(sgd.lr, lr * 0.1)
             print("lr changed to {}".format(lr * 0.1))
-        return K.get_value(c_sgd.lr)
+        return K.get_value(sgd.lr)
     
-    def f_scheduler(epoch):
-        if epoch % 10 == 0 and epoch != 0:
-            lr = K.get_value(f_sgd.lr)
-            K.set_value(f_sgd.lr, lr * 0.1)
-            print("lr changed to {}".format(lr * 0.1))
-        return K.get_value(f_sgd.lr)
-
-    c_sgd = optimizers.SGD(lr=lr, momentum=momentum, decay=decay, nesterov=False)
-    f_sgd = optimizers.SGD(lr=lr, momentum=momentum, decay=decay, nesterov=False)
+    sgd = optimizers.SGD(lr=lr, momentum=momentum, decay=decay, nesterov=False)
+    
     p_train = p_train
     width = width
     height = height
@@ -172,42 +160,29 @@ def train_model(data_path, label_path, weights_path, lr=0.001, momentum=0.9, dec
         steps = len(x_val) // batch_size
     else:
         steps = len(x_val) // batch_size + 1
-    c_reduce_lr = LearningRateScheduler(c_scheduler)
-    f_reduce_lr = LearningRateScheduler(f_scheduler)
     
-    c_net = coarseNet()
-    c_net.summary()
-    c_net.compile(optimizer = c_sgd, loss = 'mean_squared_error')
-    c_net.fit_generator(generator = get_batch(x_train, label_files, batch_size, height, width), 
+    reduce_lr = LearningRateScheduler(scheduler)
+    
+    mscnn = MSCNN()
+    mscnn.summary()
+    mscnn.compile(optimizer = sgd, loss = 'mean_squared_error')
+    mscnn.fit_generator(generator = get_batch(x_train, label_files, batch_size, height, width), 
                         steps_per_epoch=steps_per_epoch, epochs = nb_epochs, validation_data = 
                         get_batch(x_val, label_files, batch_size, height, width), validation_steps = steps,
                         use_multiprocessing=True, 
-                        shuffle=False, initial_epoch=0, callbacks = [c_reduce_lr])
-    c_net.save_weights(weights_path + '/coarseNet.h5')
-    print('coarseNet generated')
+                        shuffle=False, initial_epoch=0, callbacks = [reduce_lr])
+    mscnn.save_weights(weights_path + '/mscnn.h5')
+    print('MSCNN generated')
     
-    f_net = fineNet(c_net)
-    f_net.summary()
-    f_net.compile(optimizer = f_sgd, loss = 'mean_squared_error')
-    f_net.fit_generator(generator = get_batch(x_train, label_files, batch_size, height, width), 
-                        steps_per_epoch=steps_per_epoch, epochs = nb_epochs, validation_data = 
-                        get_batch(x_val, label_files, batch_size, height, width), validation_steps = steps,
-                        use_multiprocessing=True, 
-                        shuffle=False, initial_epoch=0, callbacks = [f_reduce_lr])
-    f_net.save_weights(weights_path + '/fineNet.h5')
-    print('fineNet generated')
-    
-    return weights_path + '/coarseNet.h5', weights_path + '/fineNet.h5'
+    return weights_path + '/mscnn.h5'
 
-def Load_model(coarse_weights, fine_weights):
-    c_net = coarseNet()
-    c_net.load_weights(coarse_weights)
-    f_net = fineNet(c_net)
-    f_net.load_weights(fine_weights)
+def Load_model(weights):
+    mscnn = MSCNN()
+    mscnn.load_weights(weights)
     
-    return f_net
+    return mscnn
 
-def usemodel(f_net, hazy_image):
+def usemodel(mscnn, hazy_image):
     
     height = hazy_image.shape[0]
     width = hazy_image.shape[1]
@@ -223,7 +198,7 @@ def usemodel(f_net, hazy_image):
     
     hazy_image = cv2.resize(hazy_image, (width, height), interpolation = cv2.INTER_AREA)
     hazy_input = np.reshape(hazy_image, (1, height, width, channel))
-    trans_map = f_net.predict(hazy_input)
+    trans_map = mscnn.predict(hazy_input)
     trans_map = np.reshape(trans_map, (height, width))
     Airlight = get_airlight(hazy_image, trans_map, p)
     clear_image = get_radiance(hazy_image, Airlight, trans_map, L)
@@ -242,10 +217,10 @@ if __name__ =="__main__":
     data_path = ''
     label_path = ''
     weights_path = ''
-    coarse_weights, fine_weights = train_model(data_path, label_path, weights_path)
+    mscnn_weights = train_model(data_path, label_path, weights_path)
     
     im_path = ''
-    f_net = Load_model(coarse_weights, fine_weights)
+    mscnn = Load_model(mscnn_weights)
     
     im = cv2.imread(im_path)
-    im_dehaze = usemodel(f_net, im)
+    im_dehaze = usemodel(mscnn, im)
